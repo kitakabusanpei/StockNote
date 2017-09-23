75be9a4c6e0c14d8b1c690875507590a78209a09
渡邉江平 <kouhei.w.56@gmail.com>
2017-09-23 11:49:00 +0900

    add name selection

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,4 +4,24 @@ class Position < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :stack
+
+  #証券コードを表示
+  def position_code(stack_code)
+    stack_code.slice(0,4)
+  end
+
+  # 会社名を表示
+  def position_company(stack_company)
+    stack_company.slice(5..-1)
+  end
+
+  # 株価を表示
+  def stack_price(stack_price)
+    Stack.code.each do |stack|
+      if position_code(stack_price) = stack
+        Stack.stack_price
+      end
+    end
+  end
+  
 end
--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,4 +1,14 @@
 class Selection < ActiveRecord::Base
   belongs_to :user
   belongs_to :stack
+
+  def selection_code(stack_code)
+    stack_code.slice(0,4)
+  end
+
+  # 会社名を表示
+  def selection_company(stack_company)
+    stack_company.slice(5..-1)
+  end
+
 end
