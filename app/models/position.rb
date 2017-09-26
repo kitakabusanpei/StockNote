708bafd501880ca6dc2448b6fff893c88cadf997
渡邉江平 <kouhei.w.56@gmail.com>
2017-09-26 16:03:04 +0900

    piechart add

--- app/models/position.rb
+++ app/models/position.rb
@@ -15,4 +15,17 @@ class Position < ActiveRecord::Base
     stack_company.slice(4..14)
   end
 
+ def pie_color(color_num)
+   count = 0
+   i = 0
+   color = ['gray' ,'red', 'yellow','blue','lime','green','aqua','purple','navy','teal','fuchsia','maroon']
+   while count < color_num
+     if count == 12
+       i = 0
+     end
+     color[i]
+     i+=1
+     color+=1
+   end
+ end
 end
