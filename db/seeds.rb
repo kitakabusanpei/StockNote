2023aaa9e26298c210a1dc041169129a010f2edb
渡邉江平 <kouhei.w.56@gmail.com>
2017-09-28 20:08:45 +0900

    pie chart add

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# require "csv"
-#
-# CSV.foreach('db/stacks.csv', headers: true) do |row|
-#   # モデル名を
-#   Company.create(:code => row[0], :name => row[1], :price_day => [2], :height_price_day => [3])
-# end
+n = 1
+while n <= 10
+  if n == 1
+  User.create!(email: "a@a",
+               password: "aaaaaa",
+               password_confirmation: "aaaaaa",
+               name: "aa",
+               uid: "f10d0a2f-1a86-4cf3-9874-962482933d3d",
+               )
+  end
+  Position.create(
+                  name: "1301 極洋",
+                  lot: n,
+                  buy_price: n,
+                  buy_reason: Faker::Beer.name,
+                  sell_price: n,
+                  sell_percent: n,
+                  sell_reason: Faker::Coffee.blend_name,
+                  user_id: 1
+                  )
+  Selection.create(
+                   selection_order: n,
+                   name: "1301 極洋",
+                   buy_selection: n,
+                   terget_price: n,
+                   user_id: 1
+                   )
+  n = n + 1
+end
