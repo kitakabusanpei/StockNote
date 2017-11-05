e2e4a356e29c0e1282185505046a8ebea553996a
渡邉江平 <kouhei.w.56@gmail.com>
2017-11-05 21:34:21 +0900

    seed-fu追加

--- db/fixtures/stacks.rb
+++ db/fixtures/stacks.rb
@@ -1,18 +1,20 @@
 # 更新用
 # "-"がどこかに含まれているため、正規表現しておくこと
+# コンソールで実行
+# ./bin/rake db:seed_fu
 
-# CSV.foreach('db/fixtures/dayStack.csv', headers: true) do |row|
-#   Stack.seed(:code) do |s|
-#     s.code = row[0]
-#     s.company = row[1]
-#     s.market = row[2]
-#     s.sector = row[3]
-#     s.market_price = row[4]
-#     s.issued_stocks = row[5]
-#     s.stock_price = row[6]
-#     s.dividend = row[7]
-#     s.industry_per = row[8]
-#     s.eps = row[9]
-#     s.bps = row[10]
-#   end
-# end
+CSV.foreach('db/fixtures/dayStack.csv', headers: true) do |row|
+  Stack.seed(:code) do |s|
+    s.code = row[0]
+    s.company = row[1]
+    s.market = row[2]
+    s.sector = row[3]
+    s.market_price = row[4]
+    s.issued_stocks = row[5]
+    s.stock_price = row[6]
+    s.dividend = row[7]
+    s.industry_per = row[8]
+    s.eps = row[9]
+    s.bps = row[10]
+  end
+end
